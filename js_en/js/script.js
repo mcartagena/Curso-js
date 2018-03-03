84808e8569f3755e7932aa2fb51e537b573ee2a0
mcartagena <marcelocartagenaorellana@gmail.com>
2018-03-03 07:15:11 -0300

    Se agrega leccion basica de eventos

--- js_en/js/script.js
+++ js_en/js/script.js
@@ -1,4 +1,27 @@
 /*
  *  Curso basico de javascript
  * 
+ *  Events - things that happen in your website
+ * 
+ *  DHTML DYNAMIC
  */
+
+ var result = document.getElementById("result");
+
+ // result.onmouseover = alert('test');  // solamente funcionara la primera vez que se carga esto es porque js ejecuta inmediatamente
+
+ // la solucion es crear una funcion anonima
+result.onmouseover = function(){
+    alert('test');
+};
+
+// una solucion mas elegante sería crear una funcion aparte que centralice todos los cambios
+
+function sample(arg){
+    alert(arg);
+};
+
+result.onmouseover = function(){
+    sample('test con funcion centralizada');
+};
+
