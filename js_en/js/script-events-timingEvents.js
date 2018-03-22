88eae1be938e10c349bcb09b7048c51bed2c24ad
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2018-03-21 21:06:50 -0400

    se modifica codigo timing Events para que funcione estilo objetos

--- js_en/js/script-events-timingEvents.js
+++ js_en/js/script-events-timingEvents.js
@@ -21,10 +21,44 @@ function stopwhachstart(stopwachHandler,startingValue){
     
 };
 
+function Stopwatch(stopwachHandler){
+    this.stopwachHandler = stopwachHandler;
+    this.startingValue;
+    this.timeOutRefId;
+    this.start = function(startingValue){
+        if(this.timeOutRefId)
+            this.stop();
+
+        this.startingValue = startingValue;
+
+        this.startStopwatch();
+    };
+    this.startStopwatch = function(){
+
+        if(this.startingValue < 0)
+            return;
+
+        this.stopwachHandler.innerHTML = this.startingValue--;
+
+        var self = this;
+
+        this.timeOutRefId = setTimeout(function(){
+            self.startStopwatch();
+        },1000);
+    };
+    this.stop = function(){
+        clearTimeout(this.timeOutRefId);
+    };
+    this.continue = function(){
+        this.startStopwatch();
+    }
+};
+
 window.onload = function(){
     
     var buttonStart = document.getElementById("buttonStart");
     var buttonStop = document.getElementById("buttonStop");
+    var buttonContinue = document.getElementById("buttonContinue");
 
     var stopwachHandler = document.getElementById("stopwachHandler");
 
@@ -54,6 +88,7 @@ window.onload = function(){
         clearInterval(intervalRefId);    
     }; */
 
+    /*
     buttonStart.onclick = function(){
 
         var startingValue = document.getElementById("startingValue").value;
@@ -65,5 +100,18 @@ window.onload = function(){
         setTimeout(function(){
             stopwhachstart(stopwachHandler,startingValue);
         }, 1000);
-    }; 
+    }; */
+
+    var stopwatch = new Stopwatch(stopwachHandler);
+
+    buttonStart.onclick = function(){
+        var startingValue = document.getElementById("startingValue").value;
+        stopwatch.start(startingValue);
+    };
+    buttonStop.onclick = function(){
+        stopwatch.stop();
+    };
+    buttonContinue.onclick = function(){
+        stopwatch.continue();
+    };
 };
