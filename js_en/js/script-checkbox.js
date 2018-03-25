dc2036f533e36422155ae16bcac047df1c5a72c7
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2018-03-24 20:46:23 -0400

    se agrega logica de radio button

--- js_en/js/script-checkbox.js
+++ js_en/js/script-checkbox.js
@@ -18,8 +18,23 @@
             if(myform.courseName[i].checked)
                 tmpString += myform.courseName[i].value + " ";
         }
-        info.innerHTML += tmpString + "<BR>";
+        info.innerHTML += tmpString + "<br>";
 
         e.preventDefault();
      };
+
+     /*
+     myform.acceptationOfToS[0].onclick = function(){
+        submitButton.disabled = false;
+     };
+     myform.acceptationOfToS[1].onclick = function(){
+        submitButton.disabled = true;
+     };
+     */
+    for(var i=0; i<myform.acceptationOfToS.length;i++){
+        myform.acceptationOfToS[i].onclick = function(){
+            submitButton.disabled = this.value === "true";    
+        }            
+    }
+     
  }
